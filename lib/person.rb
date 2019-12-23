0a56274c70b1add9321a7c87eac76dedcac45611
David Cantoran <davidcantoran36@gmail.com>
2019-12-23 03:03:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eyecolor, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attrb)
-    attrb.each {|key_obj, key_val| self.send}
+    attrb.each {|key_obj, key_val| self.send("#{key_obj}="), key_val}
   end 
 end
